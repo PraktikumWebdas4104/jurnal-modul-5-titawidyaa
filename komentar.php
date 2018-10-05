1bb2820df6f85af5a0681c75a8c94134e9109fc5
titawidyaa <43517924+titawidyaa@users.noreply.github.com>
2018-10-05 10:24:24 +0700

    Update komentar.php
    TITA WIDYA KUSUMA
    6701174008
    MI4104

--- komentar.php
+++ komentar.php
@@ -1,3 +1,20 @@
-<?php
+<form method="POST">
+	<table>
+		<tr>
+			<td>KOMENTAR</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<td><input type="submit" name="submit" value="enter"></td>
+		</tr>
+	</table>
+</form>
 
+<?php
+if (isset($_POST['submit'])) {
+	$komentar = $_POST['komentar'];
+	if (strlen($komentar)!=5 ){
+		echo "komentar yang anda masukan kurang";
+	}
+}
 ?>
