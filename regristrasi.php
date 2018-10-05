89cd23ecb6eaddcd70de29e994940723c1db087b
titawidyaa <43517924+titawidyaa@users.noreply.github.com>
2018-10-05 10:21:27 +0700

    Update regristrasi.php
    TITA WIDYA KUSUMA
    6701174008
    MI4104

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
+<body bgcolor="pink" align="center">
+<form method="POST">
+<table>
+	<tr>
+		<td>NIM :</td>
+		<td><input type="text" name="nim"></td>
+	</tr>
+	<tr>
+		<td>NAMA :</td>
+		<td><input type="text" name="nama"></td>
+	</tr>
+	<tr>
+		<td>EMAIL:</td>
+		<td><input type="email" name="email"></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td><input type="submit" name="submit" value="DAFTAR"></td>
+	</tr>
+</table>
+</form>
 </body>
-</html>
